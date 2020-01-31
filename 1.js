e0f70e5d3e84897aeeede2bdd61456226502e123
= <=>
2020-01-31 22:38:46 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,25 @@
+﻿ $(function(){
+ 	$(".content ul").isotope({
+ 		itemSelector:"li"
+ 	});
+
+ 	//code cho phan nut
+ 	$("nav ul li a").click(function(event) {
+ 		var danhmuc = $(this).data("danhmucanh");
+ 		var ten = $(this).text();
+
+ 		//dat ten vao trong h1
+
+ 		if(danhmuc == "all"){
+ 			$("section h1").text("Tất cả ảnh");
+ 			$(".content ul").isotope({filter:"*"});
+ 		}
+ 		else{
+ 			$("section h1").text(ten);
+ 			$(".content ul").isotope({filter:danhmuc});
+ 		}
+ 		
+ 		return false;
+ 	});
+})  
+ 
